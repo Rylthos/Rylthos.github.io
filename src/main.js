88ba1fb02926a07013a11d61c05776b9279b34cf
Aaron Danton <aarondanton04@hotmail.com>
2026-07-14 22:46:38 +0100

    feat: add colourscheme selector

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
 import { generateProjects } from "./generate.js";
 
 window.onload = generateProjects;
+
+document.querySelectorAll(".colour_selector").forEach(e =>
+  e.addEventListener("click", select_colour)
+)
+
+function select_colour(self, event) {
+  document.querySelectorAll(".selected").forEach(e =>
+    e.classList.remove("selected"));
+
+  self.currentTarget.classList.add("selected");
+
+  document.documentElement.dataset.theme = self.currentTarget.dataset.colourscheme
+}
